0f26a3a8ff9c9696b33e2b7edd2066b10d781282
annadowell <146206333+annadowell@users.noreply.github.com>
2024-07-25 20:30:38 +0100

    Create compile.py

---
+++ scripts/compile.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def load_data(url):
+    data = pd.read_csv(url)
+    return data
+
+archive = 'https://raw.githubusercontent.com/annadowell/streamlitMpsApp/main/test.csv'
+test1 = load_data(archive)
+
+new = 'https://raw.githubusercontent.com/annadowell/streamlitMpsApp/main/test2.csv'
+test2 = load_data(new)
+
+dataframes = [test1, test2]
+
+test3 = pd.concat(dataframes)
+
+# Save the concatenated DataFrame to a CSV file
+test3.to_csv('test3.csv', index=False)

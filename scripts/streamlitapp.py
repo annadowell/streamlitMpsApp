bd4da800ce99c850bf138994d5f246eb22fb7d14
annadowell <146206333+annadowell@users.noreply.github.com>
2024-08-05 15:46:32 +0100

    Update streamlitapp.py

--- scripts/streamlitapp.py
+++ scripts/streamlitapp.py
@@ -84,9 +84,11 @@ def CrossReferencing(keywords):
         column_config={
         "questions": st.column_config.TextColumn(
             "questions",
+            width=large, 
             ),
         "interests": st.column_config.TextColumn(
             "interests",
+            width=large, 
             ),
         },
         hide_index=True, 
